468a6a9fa829a94134543f254c5b9e87c6c3aef6
Haropis <90323820+Haropis@users.noreply.github.com>
2021-10-29 11:50:29 +0100

    update

--- mail.php
+++ mail.php
@@ -1,16 +1,43 @@
 <?php
-//get data from form
-$name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['comment'];
-$to = "ayo.haropis@haropisglobal.com.ng";
-$subject = "New Mail From haropisglobal website";
-$txt = "Name = " . $name . "\r\n Email = " . $email . "\r\n Message = " . $message;
-$headers = "From: noreply@haropisglobal.com.ng" . "\r\n" .
-"CC: somebodyelse@example.com";
-if($email!=NULL){
-    mail($to,$subject,$txt,$headers);
+use PHPMailer\PHPMailer\PHPMailer;
+
+require_once 'phpmailer/Exception.php';
+require_once 'phpmailer/PHPMailer.php';
+require_once 'phpmailer/SMTP.php';
+
+$mail = new PHPMailer(true);
+
+$alert = '';
+
+if(isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $email = $_POST[email];
+    $message = $_POST['message'];
+
+    try{
+        $mail->isSMTP(); = 'smtp.gmail.com';
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'youremail@gmail.com'; //Gmail address which you want to use as SMTP server
+        $mail->Password = 'youremailpassword'; //Gmail address password
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port = '587';
+
+        $mail->setFrom('youremail@gmail.com'); //Gmail address which you used as SMTP server
+        $mail->addAddress('youranyemail@gmail.com'); //Email address where you want to receive emails (you can use any mail address)
+
+        $mail->isHTML(true);
+        $mail->Subject = 'Message Received (Contact Page)';
+        $mail->Body = '<h3>Name : $name <br>Email : $email <br>Message : $message </h3>';
+
+        $mail->send();
+        $alert = '<div class="alert-success">
+                    <span>Message Sent! Thank you for Contacting Us.</span>
+                </div>';
+    }catch (Exception $e){
+        $alert = '<div class="alert-error">
+        <span>'.$e->getMessage().'</span>
+    </div>';
     }
-    //redirect
-    header("Location:thankyou.html");
-    ?>
+    }
+}
